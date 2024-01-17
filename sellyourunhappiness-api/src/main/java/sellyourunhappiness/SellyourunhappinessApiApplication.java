f572f28ae8435875554e4b24c02061dafd3e3e4d
JuSeong1130 <jsmoon1130@naver.com>
2024-01-17 10:46:16 +0900

    rename : application 파일 패키지 위치 상위로 변경

--- sellyourunhappiness-api/src/main/java/sellyourunhappiness/SellyourunhappinessApiApplication.java
+++ sellyourunhappiness-api/src/main/java/sellyourunhappiness/SellyourunhappinessApiApplication.java
@@ -1,13 +1,12 @@
-package sellyourunhappiness.api;
+package sellyourunhappiness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SellyourunhappinessApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SellyourunhappinessApiApplication.class, args);
     }
-
 }
+
