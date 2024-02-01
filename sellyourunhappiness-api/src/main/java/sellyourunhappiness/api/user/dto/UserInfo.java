03bbf42473cb2fc5e67c900025200cce7a3fbf8f
Park Yun Chan <dbscks9793@gmail.com>
2024-02-02 00:25:23 +0900

    refactor: SessionUser 제거

--- sellyourunhappiness-api/src/main/java/sellyourunhappiness/api/user/dto/UserInfo.java
+++ sellyourunhappiness-api/src/main/java/sellyourunhappiness/api/user/dto/UserInfo.java
@@ -1,10 +1,8 @@
 package sellyourunhappiness.api.user.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class UserInfo {
 	private String username;
 	private String email;
